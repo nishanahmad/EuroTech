06966546cfff2591889fec64b64c20ccc9c20e87
Nishan Ahmed <nishanahmad@gmail.com>
2018-09-08 12:00:06 +0530

    First commit

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php	
+	$con=mysqli_connect("localhost","sugkcwfnmb","ybwJNkgjk3","sugkcwfnmb");
+	// Check connection
+	if ($con->connect_error) 
+	{
+		die("Connection failed: " . $con->connect_error);
+	}
+?>
